306b6d0d9fc868af3d0592f92c57bb9ce761c762
Phuc Tam Nguyen <phuctam07.hue@gmail.com>
2025-09-19 18:38:31 +0700

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        int buy = prices[0];
+        int buy = INT_MAX;
+        int temp = 0;
         for (auto days : prices) {
             if (days < buy) {
                 buy = days;
-            } else {
-                if ((days - buy)> profit) profit = days - buy;
-                }
+            }
+            temp = days - buy;
+            if (profit < temp) profit = temp;
         }
         return profit;
     }
