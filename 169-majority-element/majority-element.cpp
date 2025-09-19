5eeeeaf1fb7640ea76e252348dcd654891c89bcb
Phuc Tam Nguyen <phuctam07.hue@gmail.com>
2025-09-19 17:56:23 +0700

    Time: 0 ms (100.00%) | Memory: 28 MB (86.68%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
+        for (auto x : nums) {
+            if (count == 0) {
+                candidate = x;
+                count = 1;
+            } else if (x == candidate) {
+                count++;
+            } else count--;
+        }    
+        return candidate;
+    }
+};
